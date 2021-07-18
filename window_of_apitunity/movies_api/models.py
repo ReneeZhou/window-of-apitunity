8a2985f71b16112a4e9e606bd15545147a1b1167
Renee Zhou <shanju.chou@gmail.com>
2021-07-18 23:42:38 +0800

    created movie model & made migration file

--- window_of_apitunity/movies_api/models.py
+++ window_of_apitunity/movies_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(name = 'title', max_length = 150)
+    average_rating = models.FloatField(name = 'average_rating')
+    num_votes = models.IntegerField(name = 'num_votes')
+    genres  = models.CharField(name = 'genres', max_length = 50)
+    runtime_minutes = models.IntegerField(name = 'runtime_minutes')
