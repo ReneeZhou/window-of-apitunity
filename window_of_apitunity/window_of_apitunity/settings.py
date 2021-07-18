d28d8ea619968b2883dd0a03be5742d1aa427a1f
Renee Zhou <shanju.chou@gmail.com>
2021-07-18 11:02:37 +0800

    reconfigured SECRET_KEY

--- window_of_apitunity/window_of_apitunity/settings.py
+++ window_of_apitunity/window_of_apitunity/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ljs_3y*23h8+xb)8bup!41ckko902)ml8#p6sj$!iw57@v1lp_'
+SECRET_KEY = os.getenv('APITUNITY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
