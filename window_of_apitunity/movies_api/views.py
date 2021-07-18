963b31652f13c5ba1cc9ff7b85c5d614ee592eb0
Renee Zhou <shanju.chou@gmail.com>
2021-07-18 23:43:26 +0800

    created MovieView

--- window_of_apitunity/movies_api/views.py
+++ window_of_apitunity/movies_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+
+class MovieView(viewsets.ModelViewSet):
+    """
+    API endpoint that allows movies to be added and rated. 
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
