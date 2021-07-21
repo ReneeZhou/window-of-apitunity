6857d37603b3b3922bf3297158d60c0954bc26e3
Renee Zhou <shanju.chou@gmail.com>
2021-07-22 01:10:32 +0800

    updated Movie model & made migration file

--- window_of_apitunity/movies_api/models.py
+++ window_of_apitunity/movies_api/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Movie(models.Model):
+    imdb_id = models.CharField(name = 'imdb_id', max_length = 15)
     title = models.CharField(name = 'title', max_length = 150)
-    average_rating = models.FloatField(name = 'average_rating')
-    num_votes = models.IntegerField(name = 'num_votes')
+    year = models.SmallIntegerField(name = 'year')
     genres  = models.CharField(name = 'genres', max_length = 50)
     runtime_minutes = models.IntegerField(name = 'runtime_minutes')
+    average_rating = models.FloatField(name = 'average_rating')
+    num_votes = models.IntegerField(name = 'num_votes')
